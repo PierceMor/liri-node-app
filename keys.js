4081f8383e2b6dfbf49a5be4e9ea0e22bf3b25cd
Pierce <piercemorrill91@gmail.com>
2018-03-29 20:52:22 -0500

    end of class

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.twitter = {
+    consumer_key: process.env.TWITTER_CONSUER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    consumer_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.eventNames.SPOTIFY_SECRET
+};
+
+consumer_require('./env');
