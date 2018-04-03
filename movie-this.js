5d14af3b0ae62bd6957231de047d70fcd14bddc8
Pierce <piercemorrill91@gmail.com>
2018-04-03 18:28:08 -0500

    default command prompt works

--- movie-this.js
+++ movie-this.js
@@ -3,8 +3,8 @@ let fs = require('fs-extra');
 
 let movie = process.argv.slice(2).join('+');
 
-
-    if (typeof movie === 'undefined'){
+   // thsi should set aa default command prompt
+   if (movie === '' ){
         movie = 'Frozen';
     }
 
@@ -12,13 +12,13 @@ let movie = process.argv.slice(2).join('+');
 request('http://www.omdbapi.com/?t=' + movie + '&r=json&plot=short&apikey=8b7b736c')
     .then(response=> {
         let data = JSON.parse(response);
-        console.log( 'Title of the movie: ' + data.Title);
-        console.log( 'Year the movie came out: ' + data.Year);
+        // console.log( 'Title of the movie: ' + data.Title);
+        // console.log( 'Year the movie came out: ' + data.Year);
         // console.log( 'IMDB Rating of the movie: ' + data.imdbRating);
         // console.log( 'Rotten Tomatoes Rating of the movie ' + data.Ratings[data.Ratings.length -2 ].Value);
         // console.log( 'Country where the movie was produced: ' + data.Country);
         // console.log( 'Language of the movie: ' + data.Language);
         // console.log( 'Plot of the movie: ' + data.Plot);
         // console.log( 'Actors in the movie: ' + data.Actors);
-
+        console.log(data);
     });
